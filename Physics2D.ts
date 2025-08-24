8543d3e88ff500615fdc4094613f6edbdfe82e44
dalps <git@dalp.sh>
2025-08-24 19:23:07 +0200

    (split from canvas-demos) Start working on collisions

--- Physics2D.ts
+++ Physics2D.ts
@@ -1,4 +1,4 @@
-import { popsicle } from "./CanvasUtils";
+import { circle, popsicle } from "./CanvasUtils";
 import { Point2 } from "./utils";
 
 /**
@@ -62,11 +62,74 @@ export class Repulsion extends Force {
   }
 }
 
+export class CollisionManager {
+  private static _bodies: Set<WeakRef<DynamicBody>> = new Set();
+
+  static register(body: DynamicBody): WeakRef<DynamicBody> {
+    if (!body.collider) {
+      throw new Error("Cannot register a body without an attached collider.");
+    }
+
+    if (body.ref && this._bodies.has(body.ref)) {
+      // console.log("Already has a ref")
+      return body.ref;
+    }
+
+    const ref = new WeakRef(body);
+    this._bodies.add(ref);
+    return ref;
+  }
+
+  static update(dt: number) {
+    this._bodies.forEach((ref) => {
+      const body = ref.deref();
+
+      if (!body) {
+        console.log("Body missing");
+        return;
+      }
+
+      this._bodies.forEach((ref2) => {
+        const b2 = ref2.deref();
+        if (ref === ref2 || !b2) return;
+
+        const c1 = body.collider!;
+        const c2 = b2.collider!;
+        if (body.collider!.checkContact(b2.collider!)) {
+          const direction = c1.center.sub(c2.center);
+          body.addForce(new Force(direction, 10));
+          b2.addForce(new Force(direction.multiplyScalar(-1), 10));
+        }
+      });
+    });
+  }
+}
+
+export abstract class Collider {
+  abstract checkContact(c2: Collider): boolean;
+}
+
+export class CircleCollider extends Collider {
+  constructor(public center: Point2, public radius: number) {
+    super();
+  }
+
+  checkContact(c2: CircleCollider): boolean {
+    const distance = this.center.sub(c2.center).l2();
+    const colliding = distance <= this.radius * 2;
+
+    // console.log(`${distance}, ${colliding}`);
+    return this.center.sub(c2.center).l2() <= this.radius * 2;
+  }
+}
+
 export class DynamicBody {
   public velocity: Point2;
   private _forces: Force[] = [];
   private _aux = new Point2(0, 0);
   private _locks = { x: false, y: false };
+  public collider?: CircleCollider;
+  public ref?: WeakRef<DynamicBody>;
 
   constructor(public position: Point2, public mass = 1, public friction = 0) {
     this.velocity = new Point2(0, 0);
@@ -83,6 +146,11 @@ export class DynamicBody {
 
   clearForces() {
     this._forces = [];
+  }
+
+  createCollider() {
+    this.collider = new CircleCollider(this.position, 10);
+    this.ref = CollisionManager.register(this);
   }
 
   public get totalForce() {
@@ -150,9 +218,39 @@ export class DynamicBody {
     // );
   }
 
+  drawCollider(ctx: CanvasRenderingContext2D) {
+    if (!this.collider) return;
+
+    ctx.lineWidth = 1;
+    ctx.strokeStyle = "green";
+
+    ctx.beginPath();
+    circle(ctx, this.collider?.center, this.collider?.radius);
+    ctx.closePath();
+    ctx.stroke();
+  }
+
   toString() {
     return `[m=${this.mass} p=${this.position} v=${this.velocity} a=${this.acceleration} F=${this.totalForce} forces=[${this._forces}]]`;
   }
 }
 
 export const Gravity = new Force(new Point2(0, 1), 9.81);
+
+export class Ball extends DynamicBody {
+  constructor(p: Point2, public radius = 2) {
+    super(p, radius, 0);
+  }
+
+  draw(ctx: CanvasRenderingContext2D) {
+    ctx.fillStyle = "yellow";
+    ctx.strokeStyle = "black";
+    ctx.lineWidth = 2;
+    ctx.beginPath();
+    ctx.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2);
+    ctx.closePath();
+
+    ctx.fill();
+    ctx.stroke();
+  }
+}
