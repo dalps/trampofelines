5aa8d4bfb058b229453ba895ef25da805ddf9288
dalps <git@dalp.sh>
2025-08-24 12:18:51 +0200

    (split from canvas-demos) Optimize ripples

--- Ripple.ts
+++ Ripple.ts
@@ -1,22 +1,57 @@
 import { circle } from "./CanvasUtils";
 import { damp, type Point2 } from "./utils";
 
-// TODO: ripple manager
+// TODO: ripple manager & cleanup
 
+export class RippleManager {
+  private static _ripples: Set<WeakRef<Ripple>> = new Set();
+
+  static add(ripple: Ripple) {
+    const ref = new WeakRef(ripple);
+    this._ripples.add(ref);
+    return ref;
+  }
+
+  static delete(ref: WeakRef<Ripple>) {
+    this._ripples.delete(ref);
+  }
+
+  static draw(ctx: CanvasRenderingContext2D) {
+    for (let ref of this._ripples.values()) {
+      ref.deref()?.draw(ctx);
+    }
+  }
+
+  static update(dt: number) {
+    for (let ref of this._ripples.values()) {
+      ref.deref()?.update(dt);
+    }
+  }
+
+  static updateAndDraw(ctx: CanvasRenderingContext2D, dt: number) {
+    this.update(dt);
+    this.draw(ctx);
+  }
+}
+
+const EPSILON = 0.001;
 export class Ripple {
   private _radius: number;
   private _transparency: number;
+  private _ref: WeakRef<Ripple>;
+
   constructor(
     public position: Point2,
     public initialRadius = 5,
     public finalRadius = 10,
     public initialTransparency = 1,
     public finalTransparency = 0,
-    public fillColor = "#rgb()",
+    public fillColor = "rgb(0,0,0,0.2)",
     public speed = 7
   ) {
     this._radius = initialRadius;
     this._transparency = initialTransparency;
+    this._ref = RippleManager.add(this);
   }
 
   draw(ctx: CanvasRenderingContext2D) {
@@ -35,5 +70,12 @@ export class Ripple {
       this.speed,
       dt
     );
+
+    if (
+      Math.abs(this._radius - this.finalRadius) <= EPSILON ||
+      Math.abs(this._transparency - this.finalTransparency) <= EPSILON
+    ) {
+      RippleManager.delete(this._ref);
+    }
   }
 }
